00ad4c076b11342a144c5f50b6ceeae2e04d85ad
ni-eminen <matias.nieminen@happeo.com>
2023-06-18 23:34:24 +0300

    todo app

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,128 @@
-import Image from 'next/image'
+"use client";
+import { useState } from "react";
+
+const TodoWindow = ({
+  header,
+  notes,
+  onSelect,
+}: {
+  header: string;
+  notes: string[];
+  onSelect: (str: string) => void;
+}) => {
+  return (
+    <div className="bg-blue-400 rounded-lg m-3 p-5">
+      <h1 className="text-2xl mb-2">{header}</h1>
+      <div className="mb-4">
+        {notes.map((str: string) => {
+          return (
+            <div key={str} className="flex items-center my-2">
+              <input
+                onChange={() => onSelect(str)}
+                id="default-checkbox"
+                type="checkbox"
+                value={str}
+                className="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+              />
+              <label
+                htmlFor="default-checkbox"
+                className="ml-2 text-sm font-medium text-white"
+              >
+                {str}
+              </label>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
 
 export default function Home() {
+  const [input, setInput] = useState("");
+  const [todoNotes, setTodoNotes] = useState<string[]>([]);
+  const [dailyNotes, setDailyNotes] = useState<string[]>([]);
+  const [generalNotes, setGeneralNotes] = useState<string[]>([]);
+  const [yesterdayNotes, setYesterdayNotes] = useState<string[]>([]);
+
+  const processInput = () => {
+    const code = input.slice(0, 2);
+    const note = input.slice(2, input.length);
+    switch (code) {
+      case "/t":
+        setTodoNotes([...todoNotes, note]);
+        break;
+      case "/d":
+        setDailyNotes([...dailyNotes, note]);
+        break;
+      case "/g":
+        setGeneralNotes([...generalNotes, note]);
+        break;
+      case "/y":
+        setYesterdayNotes([...yesterdayNotes, note]);
+        break;
+    }
+    setInput("");
+  };
+
+  const handleKeyDown = (event: { key: string }) => {
+    if (event.key === "Enter") {
+      processInput();
+    }
+  };
+
+  const removeTodo = (note: string) => {
+    console.log("remove todo");
+    setTodoNotes(todoNotes.filter((x) => x !== note));
+  };
+
+  const removeDaily = (note: string) => {
+    setDailyNotes(dailyNotes.filter((x) => x !== note));
+  };
+
+  const removeYesterday = (note: string) => {
+    setYesterdayNotes(yesterdayNotes.filter((x) => x !== note));
+  };
+
+  const removeGeneral = (note: string) => {
+    setGeneralNotes(generalNotes.filter((x) => x !== note));
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="flex-col max-w-full min-h-full max-h-screen bg-blue-500">
+      <div className="flex-col grid grid-cols-1 h-screen">
+        <div className="grid grid-cols-2 min-w-screen bg-blue-500 justify-between">
+          <TodoWindow
+            onSelect={removeTodo}
+            notes={todoNotes}
+            header={"Todo:"}
+          ></TodoWindow>
+          <TodoWindow
+            onSelect={removeDaily}
+            notes={dailyNotes}
+            header={"Daily:"}
+          ></TodoWindow>
+          <TodoWindow
+            onSelect={removeGeneral}
+            notes={generalNotes}
+            header={"General:"}
+          ></TodoWindow>
+          <TodoWindow
+            onSelect={removeYesterday}
+            notes={yesterdayNotes}
+            header={"Yesterday:"}
+          ></TodoWindow>
+        </div>
+        <div className="flex justify-center bg-blue-500">
+          <input
+            onKeyDown={handleKeyDown}
+            className="outline-none bg-blue-400 my-2 rounded-lg px-2 py-1.5 w-1/2 max-h-10 items-center content-center"
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+            type="text"
+          ></input>
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
     </main>
-  )
+  );
 }
