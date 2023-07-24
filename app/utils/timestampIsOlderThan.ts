114a5ab6035fbf34c4c11793674b81aa95c85f51
ni-eminen <matias.nieminen@happeo.com>
2023-07-24 10:30:20 +0300

    del: unused variable

--- app/utils/timestampIsOlderThan.ts
+++ app/utils/timestampIsOlderThan.ts
@@ -1,6 +1,5 @@
 export const timestampIsOlderThan = (timestamp: number, olderThanLimit: number = 72000000) => {
     const today = new Date(Date.now());
-    const timestampAsDate = new Date(timestamp);
   
     return today.getMilliseconds() - timestamp > olderThanLimit;
   };
